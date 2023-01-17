5ce26dc73bc954beff71bed81e1b75ddc82529ae
rs2190 <122139610+rs2190@users.noreply.github.com>
2023-01-17 13:13:30 +0000

    [Finish] 2023/01/17 4-5 devise のインストール

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+
+# 「:database_authenticatable, :registerable,」のように、devise の後ろに :（コロン）で始まる部分が devise の機能名です。
+
+# :database_authenticatable（パスワードの正確性を検証）
+# :registerable（ユーザ登録や編集、削除）
+# :recoverable（パスワードをリセット）
+# :rememberable（ログイン情報を保存）
+# :validatable（email のフォーマットなどのバリデーション）
+
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+end
